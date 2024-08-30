b5243e6eeea74f9ac115dafab7b866b70b3b8677
LAlex <leon.alex.8835@gmail.com>
2024-08-30 17:16:16 -0400

    implemenacion del modulo inscripcion

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -820,6 +820,33 @@
                                 </ul>
                             </li>
 
+
+
+                            <li class="slide has-sub">
+                                <a href="javascript:void(0);" class="side-menu__item">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon"
+                                        viewBox="0 0 24 24">
+                                        <path d="M0 0h24v24H0V0z" fill="none" />
+                                        <path d="M13 4H6v16h12V9h-5V4zm3 14H8v-2h8v2zm0-6v2H8v-2h8z" opacity=".3" />
+                                        <path
+                                            d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z" />
+                                    </svg>
+                                    <span class="side-menu__label">Inscripcion</span>
+                                    <i class="fe fe-chevron-right side-menu__angle"></i>
+                                </a>
+                                <ul class="slide-menu child1">
+                                    <li class="slide side-menu__label1">
+                                        <a href="javascript:void(0);">Inscripcion</a>
+                                    </li>
+                                    <li class="slide">
+                                        <a href="grid-tables.html" class="side-menu__item">inscripcion a membresias</a>
+                                    </li>
+                                    <li class="slide">
+                                        <a href="data-tables.html" class="side-menu__item">Historial de inscripciones</a>
+                                    </li>
+                                </ul>
+                            </li>
+
                             <li class="slide has-sub">
                                 <a href="javascript:void(0);" class="side-menu__item">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon"
